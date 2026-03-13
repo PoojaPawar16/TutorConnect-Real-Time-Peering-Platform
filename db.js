09caaebae04053aba8a08166b7b186eead8d8738
Pooja Pawar <pp5064492@gmail.com>
2026-03-13 17:48:45 +0530

    use env database

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "skill@123",
-  database: "tutorconnect",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0,
 });
 
 console.log("✅ MySQL Pool Created");
