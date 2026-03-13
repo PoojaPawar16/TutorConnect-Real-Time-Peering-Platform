0b979816a2db8ea450599b20b170ff97323c7589
Pooja Pawar <pp5064492@gmail.com>
2026-03-13 18:05:41 +0530

    fix database config for production

--- db.js
+++ db.js
@@ -1,15 +1,19 @@
 const mysql = require("mysql2");
 
-const db = mysql.createPool({
+const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  waitForConnections: true,
-  connectionLimit: 10,
 });
 
-console.log("✅ MySQL Pool Created");
+db.connect((err) => {
+  if (err) {
+    console.error("❌ Database connection failed:", err.message);
+    return;
+  }
+  console.log("✅ Connected to MySQL Database!");
+});
 
 module.exports = db;
